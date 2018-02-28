187f36299cb997ccb9042e44298a4d8395de67e3
lzq122 <17865912453@163.com>
2018-02-28 17:43:38 +0800

    Add files via upload

---
+++ infor/datas/nav.js
@@ -0,0 +1,21 @@
+var navs = [ {
+	"title": "规模",
+	"icon": "fa-cogs",
+	"href": "scale.html",
+	"spread": false,
+},{
+	"title": "影响力",
+	"icon": "fa-cogs",
+	"href": "influence.html",
+	"spread": false,
+}, {
+	"title": "发展",
+	"icon": "&#x1002;",
+	"href": "development.html",
+	"spread": false,
+},{
+	"title": "基金",
+	"icon": "fa-stop-circle",
+	"href": "funds.html",
+	"spread": false
+}];
